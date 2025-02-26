f4c66dae2d431d8acb9ca47d6ba318df0252f62c
www.ming115 <www.ming115@gmail.com>
2025-02-26 15:14:46 +0900

    디자인 이미지 팝업 업로드

--- script/index.js
+++ script/index.js
@@ -12,3 +12,29 @@ const snsSwiper = new Swiper('.sns_swiper', {
     slidesPerView:4,
     spaceBetween:30,
 })
+/* other design 팝업 */
+const img_popup_bg = document.querySelector('.img_popup_bg')
+const bnrImg = document.querySelectorAll('.bnr_swiper img')
+const snsImg = document.querySelectorAll('.sns_swiper img')
+console.log(bnrImg)
+
+img_popup_bg.style.display = 'none';
+for(let i of bnrImg){
+    i.addEventListener('click',()=>{
+        img_popup_bg.style.display = 'block';
+        console.log(i.src)
+        img_popup_bg.children[0].children[0].src = i.src;
+        img_popup_bg.children[0].style.width = i.src;
+    })
+}
+for(let i of snsImg){
+    i.addEventListener('click',()=>{
+        img_popup_bg.style.display = 'block';
+        console.log(i.src)
+        img_popup_bg.children[0].children[0].src = i.src;
+        img_popup_bg.children[0].style.width = i.src;
+    })
+}
+img_popup_bg.addEventListener('click',()=>{
+    img_popup_bg.style.display = 'none'
+})
