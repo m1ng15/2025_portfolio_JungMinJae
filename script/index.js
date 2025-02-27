72d510d3a5a864ed43f8a7ea2158229cd7899461
www.ming115 <www.ming115@gmail.com>
2025-02-27 16:27:06 +0900

    250227 포트폴리오 web design 수정

--- script/index.js
+++ script/index.js
@@ -12,11 +12,19 @@ const snsSwiper = new Swiper('.sns_swiper', {
     slidesPerView:4,
     spaceBetween:30,
 })
+
+const detailSwiper = new Swiper('.detail_swiper', {
+    autoplay:{delay:0,},
+    loop:true,
+    speed: 7000,
+    slidesPerView:4,
+    spaceBetween:30,
+})
 /* other design 팝업 */
 const img_popup_bg = document.querySelector('.img_popup_bg')
 const bnrImg = document.querySelectorAll('.bnr_swiper img')
 const snsImg = document.querySelectorAll('.sns_swiper img')
-console.log(bnrImg)
+const detailImg = document.querySelectorAll('.detail_swiper img')
 
 img_popup_bg.style.display = 'none';
 for(let i of bnrImg){
@@ -35,6 +43,15 @@ for(let i of snsImg){
         img_popup_bg.children[0].children[0].src = i.src;
         img_popup_bg.children[0].style.width = '700px';
         img_popup_bg.children[0].style.marginTop = '150px';
+    })
+}
+for(let i of detailImg){
+    i.addEventListener('click',()=>{
+        img_popup_bg.style.display = 'block';
+        console.log(i.src)
+        img_popup_bg.children[0].children[0].src = i.src;
+        img_popup_bg.children[0].style.width = '450px';
+        img_popup_bg.children[0].style.marginTop = '65px';
     })
 }
 img_popup_bg.addEventListener('click',()=>{
