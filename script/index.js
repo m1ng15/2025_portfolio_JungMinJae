1c7dfc43977de164ed73dcd652944d475e11f5b0
www.ming115 <www.ming115@gmail.com>
2025-03-12 15:37:03 +0900

    웹 디자인 작업물 슬라이드 수정

--- script/index.js
+++ script/index.js
@@ -2,8 +2,7 @@ const bnrSwiper = new Swiper('.bnr_swiper', {
     autoplay:{delay:0,},
     loop:true,
     speed: 5000,
-    slidesPerView:4,
-    spaceBetween:30,
+    slidesPerView: 4,
 })
 const snsSwiper = new Swiper('.sns_swiper', {
     autoplay:{delay:0,},
@@ -14,10 +13,10 @@ const snsSwiper = new Swiper('.sns_swiper', {
 })
 
 const detailSwiper = new Swiper('.detail_swiper', {
-    autoplay:{delay:0,},
+    /* autoplay:{delay:0,}, */
     loop:true,
-    speed: 7000,
-    slidesPerView:4,
+    /* speed: 7000, */
+    slidesPerView:2,
     spaceBetween:30,
 })
 /* other design 팝업 */
@@ -30,28 +29,30 @@ img_popup_bg.style.display = 'none';
 for(let i of bnrImg){
     i.addEventListener('click',()=>{
         img_popup_bg.style.display = 'block';
-        console.log(i.src)
         img_popup_bg.children[0].children[0].src = i.src;
-        img_popup_bg.children[0].style.width = '900px';
+        img_popup_bg.children[0].style.width = '750px';
         img_popup_bg.children[0].style.marginTop = '250px';
     })
 }
 for(let i of snsImg){
     i.addEventListener('click',()=>{
         img_popup_bg.style.display = 'block';
-        console.log(i.src)
         img_popup_bg.children[0].children[0].src = i.src;
+        console.log(i.height)
         img_popup_bg.children[0].style.width = '700px';
         img_popup_bg.children[0].style.marginTop = '150px';
+        if(i.height > 419){
+            console.log('hell0');
+            img_popup_bg.children[0].style.marginTop = '45px';
+        }
     })
 }
 for(let i of detailImg){
     i.addEventListener('click',()=>{
         img_popup_bg.style.display = 'block';
-        console.log(i.src)
         img_popup_bg.children[0].children[0].src = i.src;
-        img_popup_bg.children[0].style.width = '450px';
-        img_popup_bg.children[0].style.marginTop = '65px';
+        img_popup_bg.children[0].style.width = '800px';
+        img_popup_bg.children[0].style.marginTop = '45px';
     })
 }
 img_popup_bg.addEventListener('click',()=>{
